f8a89167339f723eda531f9df4ff2794cde4d35e
zhong-1230 <64020952+zhong-1230@users.noreply.github.com>
2020-08-13 21:28:39 +0800

    初次設定

---
+++ CSharp_API_IF/Assets/wasd.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class wasd : MonoBehaviour
+{
+    public Animator anin;
+
+    public bool wating;
+    public bool running;
+    
+    public float player_speed;
+
+    private void start()
+    {
+        anin = GetComponent<Animator>();
+
+        wating = true;
+        running = false;
+
+        player_speed = 3f;
+    }
+
+
+
+    private void Control()
+    {
+        
+    
+
+}
