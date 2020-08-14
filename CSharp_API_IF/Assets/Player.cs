1a55c095c4c0e7a558cfc093843348e61e68fdac
zhong-1230 <64020952+zhong-1230@users.noreply.github.com>
2020-08-14 18:12:16 +0800

    完成W前進

---
+++ CSharp_API_IF/Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("移動速度"),Range(0,100)]
+    public float speed = 0.5f;
+
+    public Animator ani;
+
+
+    private void Start()
+    {
+        
+    }
+
+
+
+    private void Move()
+    {
+        bool W = Input.GetKey(KeyCode.W);
+        ani.SetBool("走路開關", W);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0,0,speed * Time.deltaTime);
+        }
+          
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+}
